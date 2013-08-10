0cd08d764706eb7b1dfe49851b5584580b48fd1f
Daniel Neuhäuser <dasdasich@gmail.com>
2013-08-10 17:50:56 +0200

    Release 0.1.0

--- relief/__init__.py
+++ relief/__init__.py
@@ -15,7 +15,7 @@ from relief.schema.mappings import Dict, OrderedDict, Form
 from relief.schema.sequences import Tuple, List
 
 
-__version__ = "0.1.0-dev"
+__version__ = "0.1.0"
 __version_info__ = (0, 1, 0)
 
 
